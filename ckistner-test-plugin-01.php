9a0e75cccac518cdcad62d53925bd4d60d623363
Christopher Kistner <chris.kistner0@gmail.com>
2019-03-22 13:09:42 -0700

    Finished adding basic information to PHP execution file

--- ckistner-test-plugin-01.php
+++ ckistner-test-plugin-01.php
@@ -6,5 +6,11 @@
 
  /*
   Plugin Name: Test Plugin 1
-  Plugin URI: http://
+  Plugin URI: https://github.com/chriskistner/ckistner-test-plugin-01
+  Description: First attempt at a Wordpress Plugin in prep for client requests
+  Version: 1.0.0
+  Author: Christopher Kistner
+  Author URI: http://chriskistner.surge.sh/index.html
+  License: GPLv2 or later
+  Text Domain: ckistner-plugin-01
   */
